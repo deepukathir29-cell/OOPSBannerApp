d3dabb7e645be7224c6091930dbed9431149c48c
deepukathir29 <deepukathir29@gmail.com>
2026-03-02 13:55:04 +0530

    To store character pattern in a class

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -4,58 +4,68 @@
  * Using an inner static class and utility methods.
  */
  public class OOPSBannerApp {
+
     static class CharacterPatternMap {
-        private final Character character;
+
+        private final char character;
         private final String[] pattern;
-        public CharacterPatternMap(Character character, String[] pattern) {
+
+        public CharacterPatternMap(char character, String[] pattern) {
             this.character = character;
             this.pattern = pattern;
         }
-        public Character getCharacter() {
+
+        public char getCharacter() {
             return character;
         }
+
         public String[] getPattern() {
             return pattern;
         }
     }
+
     public static CharacterPatternMap[] createCharacterPatternMaps() {
+
         String[] oPattern = {
-                " *** ",
+                "  ***  ",
+                " *   * ",
                 "*     *",
                 "*     *",
                 "*     *",
-                "*     *",
-                "*     *",
-                " *** "
+                " *   * ",
+                "  ***  "
         };
+
         String[] pPattern = {
-                "** ",
-                "*     *",
-                "*     *",
-                "** ",
-                "*      ",
-                "*      ",
-                "*      "
+                " ***** ",
+                " *    *",
+                " *    *",
+                " ***** ",
+                " *     ",
+                " *     ",
+                " *     "
         };
+
         String[] sPattern = {
-                " *** ",
-                "*      ",
-                "*      ",
-                " *** ",
+                "  *****",
+                " *     ",
+                " *     ",
+                "  *****",
                 "      *",
                 "      *",
-                " *** "
+                " ***** "
         };
 
         String[] spacePattern = {
-                "   ",
-                "   ",
-                "   ",
-                "   ",
-                "   ",
-                "   ",
-                "   "
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       ",
+                "       "
         };
+
         return new CharacterPatternMap[]{
                 new CharacterPatternMap('O', oPattern),
                 new CharacterPatternMap('P', pPattern),
@@ -63,33 +73,34 @@
                 new CharacterPatternMap(' ', spacePattern)
         };
     }
+
     public static String[] getCharacterPattern(char ch, CharacterPatternMap[] charMaps) {
+
         for (CharacterPatternMap map : charMaps) {
             if (map.getCharacter() == Character.toUpperCase(ch)) {
                 return map.getPattern();
             }
         }
-        for (CharacterPatternMap map : charMaps) {
-            if (map.getCharacter() == ' ') {
-                return map.getPattern();
-            }
-        }
-        return new String[7];
+
+        return charMaps[charMaps.length - 1].getPattern();
     }
+
     public static void printMessage(String message, CharacterPatternMap[] charMaps) {
-        final int HEIGHT = 7;
-        for (int row = 0; row < HEIGHT; row++) {
-           StringBuilder lineBuilder = new StringBuilder();
+
+        for (int row = 0; row < 7; row++) {
+
             for (int i = 0; i < message.length(); i++) {
                 String[] pattern = getCharacterPattern(message.charAt(i), charMaps);
-                lineBuilder.append(pattern[row]).append("  ");
+                System.out.print(pattern[row] + "   ");
             }
-            System.out.println(lineBuilder);
+
+            System.out.println();
         }
     }
+
     public static void main(String[] args) {
+
         CharacterPatternMap[] charMaps = createCharacterPatternMaps();
-        String message = "OOPS";
-        printMessage(message, charMaps);
+        printMessage("OOPS", charMaps);
     }
 }
